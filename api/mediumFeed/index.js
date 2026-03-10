a55408cb47ccb8daa8d6d9c5ad004e02f0b38b52
ghangasrakhi <ghangas.rakhi@gmail.com>
2026-03-10 15:54:33 +1100

    Initial commit without secrets

--- api/mediumFeed/index.js
+++ api/mediumFeed/index.js
@@ -10,7 +10,7 @@ const apiKey = AZURE_SEARCH_KEY;
 const searchClient = new SearchClient(searchEndpoint, indexName, new AzureKeyCredential(apiKey));
 
 module.exports = async function(context, req) {
-    const mediumRSS = "https://medium.com/feed/ghangas-rakhi";
+    const mediumRSS = "https://medium.com/feed/@ghangas-rakhi";
     try {
         const feed = await parser.parseURL(mediumRSS);
         const posts = feed.items.slice(0, 6).map(item => ({
@@ -34,3 +34,50 @@ module.exports = async function(context, req) {
         context.res = { status: 500, body: { error: "Failed to fetch Medium posts", details: err.message } };
     }
 };
+
+
+// const Parser = require("rss-parser");
+// const parser = new Parser();
+
+// module.exports = async function (context, req) {
+//   try {
+
+//     const feed = await parser.parseURL(
+//       "https://medium.com/feed/@ghangas-rakhi"
+//     );
+
+//     const posts = feed.items.slice(0, 5).map(post => {
+
+//       // extract image from content
+//       let thumbnail = null;
+//       const match = post.content.match(/<img[^>]+src="([^">]+)"/);
+
+//       if (match && match[1]) {
+//         thumbnail = match[1];
+//       }
+
+//       return {
+//         title: post.title,
+//         link: post.link,
+//         pubDate: post.pubDate,
+//         description: post.contentSnippet,
+//         thumbnail: thumbnail
+//       };
+//     });
+
+//     context.res = {
+//       status: 200,
+//       body: posts
+//     };
+
+//   } catch (error) {
+
+//     context.res = {
+//       status: 500,
+//       body: {
+//         error: "Failed to fetch Medium posts",
+//         details: error.message
+//       }
+//     };
+//   }
+// };
